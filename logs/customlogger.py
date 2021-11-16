5db962ebafdc33d7d34ececd84d76765e2ea7eae
maike reis <maikerdralcantata@gmail.com>
2021-11-16 11:52:20 -0300

    change logs timezone to utc

--- logs/customlogger.py
+++ logs/customlogger.py
@@ -3,7 +3,7 @@ from loguru import logger
 logger.remove()
 fmt = "<yellow><level>{level: <9}</level></yellow> \
 <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> \
-msg = <level>{message}</level>  at {time:YYYY-MM-DD HH:mm:ss.SSS}"
+msg = <level>{message}</level>  at {time:YYYY-MM-DD HH:mm:ss.SSS - zz!UTC}"
 
 """ logger.add("logs/data/i_know_what_you_did_last_summer.log",
            retention="10 days", rotation="500 mb", format=fmt, level='TRACE') """
